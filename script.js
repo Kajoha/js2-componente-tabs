c6c204dde6bc0e8258c3f73bb01c7a9d60f3c423
Karen Méndez <kmendezgz@gmail.com>
2020-09-16 08:36:47 -0600

    trabajo en clase dos

--- script.js
+++ script.js
@@ -1,28 +1,47 @@
 
-function active (event){
-
+/*function active (event){
     event.preventDefault();
     const id = event.currentTarget.getAttribute('href').substring(1); 
     
     for (let i = 0; i < conten.length; i++) {
-        conten[i].style.display = 'none';
+        conten[i].classList.add('js-content-hidden');
         if (conten[i].getAttribute('id') == id) {
-            conten[i].style.display = 'block';
+            conten[i].classList.remove('js-content-hidden');
         }
     }
 
-    // el substring es para seleccionar elementos de un string a partir de determinada posición
-}
+    for (let i = 0; i < link.length; i++) {
+        link[i].classList.remove('js-active');
+    }
+
+    event.currentTarget.classList.add('js-active');
+}*/
 
 const link = document.querySelectorAll('.tabs-tablist-item');
 const conten = document.querySelectorAll('.tabs-content-item');
 
 for (let i = 0; i < conten.length; i++) {
-    conten[i].style.display = 'none';
+    conten[i].classList.add('js-content-hidden');
 }
 
-conten[0].style.display = 'block';
+conten[0].classList.remove('js.active');
+link[0].classList.add('js-active');
 
 for (let i = 0; i < link.length; i++) {
-    link[i].addEventListener('click', active);
+    link[i].addEventListener('click', (event) => {
+        event.preventDefault();
+        const id = event.currentTarget.getAttribute('href').substring(1);
+        console.log(id);
+        //ocultamos todo menos el elemento con el id correcto.
+        for (let i = 0; i < conten.length; i++) {
+            conten[i].classList.add('js-content-hidden');
+            if (conten[i].getAttribute('id') == id) {
+                conten[i].classList.remove('js-content-hidden');
+            }
+        }
+        for (let i = 0; i < link.length; i++) {
+            link[i].classList.remove('js-active');
+        }
+        event.currentTarget.classList.add('js-active');
+    });
 }
